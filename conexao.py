232b9997ddd7bebeefd8af2ef7cae4b40ae432d7
Samuel Soares da Silva <1801144@SALAS.AULAS>
2018-08-22 11:07:00 -0300

    teste

--- conexao.py
+++ conexao.py
@@ -1,2 +1,13 @@
 def mensagem():
     print('criado no python')
+
+def somLista(lista,listona):
+    index = 0
+    resultado = []
+    for x in lista:
+        resul = x + listona[index]
+        resultado.append(resul)
+        index += 1
+    return resultado
+    
+    
